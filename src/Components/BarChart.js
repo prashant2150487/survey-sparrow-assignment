192f4e665b4dd3376d9c64c5f118619b20f69fb2
Prashnt Sachan <swepra04@gmail.com>
2024-06-02 17:25:08 +0530

    responsive ness updated

--- src/Components/BarChart.js
+++ src/Components/BarChart.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const BarChart = ({ data, maxCount }) => {
     return (
-        <div className="flex min-h-[350px] gap-x-5 w-full border-b-4 border-l-4 border-white text-white">
+        <div className="flex min-h-[350px] justify-start  w-full border-b-4 border-l-4 border-white text-white">
             {data.map((count, index) => (
                 <div key={index} className="flex flex-col justify-end ml-5">
                     <div
-                        className="w-12 border transition-all duration-300 ease-in-out rounded-xl text-center"
+                        className="w-9 md:w-11  border transition-all duration-300 ease-in-out rounded-xl text-center"
                         style={{ height: `${(count / maxCount) * 100}%` }}
                     >
                         <span className="text-white text-xs font-bold pl-1">{count}</span>
--- src/Components/index.js
+++ src/Components/index.js
@@ -9,21 +9,19 @@ const LayOut = () => {
         const newRatings = ratings.map(() => Math.floor(Math.random() * (maxCount + 1)));
         setRatings(newRatings);
     };
-
-   
     return (
         <div className="container mx-auto px-4 py-8 max-w-xl">
-            <h1 className="text-3xl font-bold mb-6">Rating Breakdown Analysis</h1>
+            <h1 className="text-xl md:text-3xl font-bold mb-6">Rating Breakdown Analysis</h1>
             <div className="shadow-lg p-6 bg-black">
                 <div className='flex text-white'>
-                    <div style={{ writingMode: 'vertical-lr', textOrientation: 'mixed' }}>
-                        <span className="font-semibold vertical">X-Axis:</span> Rating Scale (1-5)
+                    <div style={{ writingMode: 'vertical-lr', textOrientation: 'mixed', textAlign: 'center' }}>
+                        <span className="vertical text-blue-600 font-bold">X-Axis:Rating Scale </span>
                     </div>
                     <BarChart data={ratings} maxCount={maxCount} />
                 </div>
                 <span className='block text-center mt-10 font-bold text-blue-600 text-xl'>Rating</span>
                 <button
-                    className="mt-6 border border-green-500 font-bold py-2 px-4 rounded transition duration-300 ease-in-out block ml-auto text-green-500"
+                    className="mt-6 border border-green-500 font-bold md:py-2 md:px-4 px-1 py-2  rounded transition duration-300 ease-in-out block ml-auto text-green-500"
                     onClick={regenerateRatings}
                 >
                     Regenerate
